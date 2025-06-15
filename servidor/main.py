426a6626772caf8abc26c451742b02e17c55cb50
samuel_gg <samuel.escuela03@gmail.com>
2025-06-15 14:51:09 -0600

    add main template

--- servidor/main.py
+++ servidor/main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    name="Pepe"
+    return render_template('main.html',person=name)
+
+@app.route("/health")
+def healthcheck():
+    x=1+1
+    return "<p>server is up and running</p>"   
